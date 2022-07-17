9e03321bcc6387f491fe873a49d966ba21cf4f1c
Maris <marisreji2001@gmail.com>
2022-07-17 11:11:35 +0530

    Created groupchat activity and layout

---
+++ app/src/main/java/com/jns/codelink/activities/GroupChatActivity.kt
@@ -0,0 +1,11 @@
+package com.jns.codelink.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GroupChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_group_chat)
+    }
+}
